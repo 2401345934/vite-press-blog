741904ea6ba93407419302f6f28d43312b534c9b
alan.xiao <alan.xiao@d1m.cn>
2022-10-23 22:51:58 +0800

    计算机网络知识

--- docs/.vitepress/nav.js
+++ docs/.vitepress/nav.js
@@ -4,7 +4,7 @@ module.exports = [
   { text: "源码", link: "/source/react/render/", activeMatch: '/source/' },
   { text: "utils", link: "/utils/utils/business-utils/", activeMatch: '/utils/' },
   { text: "浏览器", link: "/browser/browser/composition/", activeMatch: '/browser/' },
-  { text: "计算机网络&&HTTP", link: "/http/http-https/", activeMatch: '/http/' },
+  { text: "计算机&&HTTP", link: "/http/http-https/", activeMatch: '/http/' },
   { text: "算法&&数据结构", link: "/data-structures-algorithms/introduction/", activeMatch: '/data-structures-algorithms/' },
   { text: "软能力", link: "/soft-power/asking-questions/", activeMatch: '/soft-power/' },
   { text: "工作工具", link: "/tool/compressed-image/", activeMatch: '/tool/' },
--- docs/.vitepress/routers/http.js
+++ docs/.vitepress/routers/http.js
@@ -25,31 +25,44 @@ module.exports = [
     ],
   },
   {
-    text: '计算器网络',
+    text: '计算机',
     items: [
       {
-        text: '计算机网络分层结构',
-        link: '/http/structure-computer-networks/',
+        text: '计算机网络',
+        items:[
+          {
+            text: '计算机网络基础知识',
+            link: '/http/computer-networks/',
+          },
+          {
+            text: '计算机网络分层结构',
+            link: '/http/structure-computer-networks/',
+          },
+          {
+            text: 'TCP三次握手 TCP 四次挥手',
+            link: '/http/tcp-three-tcp-four/',
+          },
+          {
+            text: 'TCP/IP / 如何保证数据包传输的有序可靠？',
+            link: '/http/orderly-reliable-transmission/',
+          },
+          {
+            text: 'TCP和UDP的区别',
+            link: '/http/tcp-udp/',
+          },
+          {
+            text: 'TCP',
+            link: '/http/tcp/',
+          },
+          {
+            text: 'UDP',
+            link: '/http/udp/',
+          },
+        ]
       },
       {
-        text: 'TCP三次握手 TCP 四次挥手',
-        link: '/http/tcp-three-tcp-four/',
-      },
-      {
-        text: 'TCP/IP / 如何保证数据包传输的有序可靠？',
-        link: '/http/orderly-reliable-transmission/',
-      },
-      {
-        text: 'TCP和UDP的区别',
-        link: '/http/tcp-udp/',
-      },
-      {
-        text: 'TCP',
-        link: '/http/tcp/',
-      },
-      {
-        text: 'UDP',
-        link: '/http/udp/',
+        text: '计算机组成原理',
+        link: '/http/computer-composition/',
       },
     ],
   },
